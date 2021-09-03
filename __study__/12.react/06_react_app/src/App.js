65e0ffbe1c24ecaa860a6797a90d8c6d26e855a2
i.nagashima <i.nagashima@refact.co.jp>
2021-09-03 18:09:59 +0900

    react

---
+++ __study__/12.react/06_react_app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.title = props.title;
+    this.message = props.message;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='bg-primary text-white display-4'>React</h1>
+        <div className='container'>
+          <p className='subtitle'>{this.title}</p>
+          <p>{this.message}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
