4f9060c987d33577cbeb7f6c0800a491abe65808
i.nagashima <i.nagashima@refact.co.jp>
2021-09-06 20:27:50 +0900

    __study__

---
+++ __study__/12.react/10_react_app/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function AlertMessage(props) {
+  return (
+    <div className='alert alert-primary h5 text-primary'>
+      <h5>{props.msg}</h5>
+    </div>
+  );
+}
+
+function App() {
+  const [val, setVal] = useState(0);
+  const [msg, setMsg] = useState('set a number...');
+
+  const doChange = (event) => {
+    setVal(event.target.value);
+  };
+
+  useEffect(() => {
+    let total = 0;
+    for (let i = 0; i <= val; i++) {
+      total += i;
+    }
+    setMsg('total:' + total + '.');
+  });
+
+  return (
+    <div>
+      <h1 className='bg-primary text-white display-4'>React</h1>
+      <div className='container'>
+        <h4 className='my-3'>Hooks sample</h4>
+        <AlertMessage msg={msg}></AlertMessage>
+        <div className='form-group'>
+          <label>input:</label>
+          <input
+            type='number'
+            className='form-control'
+            onChange={doChange}
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
